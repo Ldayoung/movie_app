880b87c5a9389e2795607f84ad0548918d4aa8e2
dayoung-lee <dimmaculatey@gmail.com>
2020-06-24 20:42:21 +0900

    Updates

---
+++ precache-manifest.26221375a2e1c1496f4ea91d1e52cf30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0099ef98414d345299287f003eae2945",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "672b26fccf8c84d4c122",
+    "url": "/movie_app/static/css/main.27146762.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "672b26fccf8c84d4c122",
+    "url": "/movie_app/static/js/main.cda39088.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
